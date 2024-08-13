f7ddd65f2b768df42067e6474293d678d666e3db
jiangshan.zhao <jiangshan.zhao@nine.com.au>
2024-08-13 21:39:20 +1000

    bugfix: fix sidebar menu close button disppear when setting parent element with overflow hidden

--- src/features/editor/components/tool-sidebar-close.tsx
+++ src/features/editor/components/tool-sidebar-close.tsx
@@ -1,14 +1,24 @@
 import { ChevronsLeft } from "lucide-react";
+import { ActiveTool } from "@/features/editor/types";
+import { cn } from "@/lib/utils";
 
 interface ToolSidebarCloseProps {
+  activeTool: ActiveTool;
   onClick: () => void;
 }
 
-export const ToolSidebarClose = ({ onClick }: ToolSidebarCloseProps) => {
+export const ToolSidebarClose = ({ onClick, activeTool }: ToolSidebarCloseProps) => {
   return (
     <button
       onClick={onClick}
-      className="absolute -right-[1.80rem] h-[70px] bg-white top-1/2 transform -translate-y-1/2 flex items-center justify-center rounded-r-xl px-1 pr-2 border-r border-y group"
+      className={cn(
+        "absolute z-[60] h-[70px] bg-white top-1/2 flex items-center justify-center rounded-r-xl px-1 pr-2 border-r border-y group",
+        "transition-all duration-300 ease-in-out",
+        activeTool === "shapes" 
+          ? "opacity-100  pointer-events-auto -translate-x-full" 
+          : "opacity-0  pointer-events-none translate-x-0 ",
+        activeTool === "shapes" ?  "left-[30.5rem]" : "left-0"
+      )}
     >
       <ChevronsLeft className="size-4 text-black group-hover:opacity-75 transition" />
     </button>
